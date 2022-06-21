40a7ede2357612f6062695cb9a72ff8ddca21df1
matsu_friends <pikapikan0526@gmail.com>
2022-06-21 14:16:41 +0900

    Init

---
+++ MornHierarchy/Mono/MornHierarchy.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+namespace MornHierarchy {
+    public class MornHierarchy : MonoBehaviour {
+        [ColorUsage(false)]public Color BackColor;
+        public bool ApplyChildren;
+        #if UNITY_EDITOR
+        private void OnValidate() {
+            UnityEditor.EditorApplication.RepaintHierarchyWindow();
+        }
+        #endif
+    }
+}
